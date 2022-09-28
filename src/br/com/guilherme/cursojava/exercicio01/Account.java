4586e0e6e921b09402d2d7b6f714f14a3c417bf6
Guilherme Viana <guilhermeviana1212@gmail.com>
2022-09-28 20:35:02 -0300

    exercício sobre construtores

---
+++ src/br/com/guilherme/cursojava/exercicio01/Account.java
@@ -0,0 +1,50 @@
+package br.com.guilherme.cursojava.exercicio01;
+
+public class Account {
+	private String holder;
+	private int number;
+	private double balance;
+	
+	public Account(String holder, int number) {
+		this.holder = holder;
+		this.number = number;
+	}
+
+	public Account(String holder, int number, double initialDeposit) {
+		this.holder = holder;
+		this.number = number;
+		deposit(initialDeposit);
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount + 5;
+	}
+
+	@Override
+	public String toString() {
+		return "Account " + number +", Holder: " + holder + ", Balance: " + String.format("%.2f", balance);
+	}
+	
+	
+}
+
---
+++ src/br/com/guilherme/cursojava/exercicio01/Main.java
@@ -0,0 +1,60 @@
+package br.com.guilherme.cursojava.exercicio01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		Integer number = sc.nextInt();
+		System.out.print("Enter accont holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		
+		System.out.print("Is there na initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+		
+		if(response == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(holder, number, initialDeposit);
+			
+		}else {
+			account = new Account(holder, number, 0.0);
+		}
+		
+		System.out.println();
+		System.out.println("Account date: ");
+		System.out.println(account);
+		System.out.println();
+		
+		System.out.print("Enter a deposit value: ");
+		double amount = sc.nextDouble();
+		account.deposit(amount);
+		
+		System.out.println();
+		System.out.println("Update account date: ");
+		System.out.println(account);
+		System.out.println();
+		
+		System.out.print("Enter a withdraw value: ");
+		amount = sc.nextDouble();
+		account.withdraw(amount);
+		
+		System.out.println();
+		System.out.println("Update account date: ");
+		System.out.println(account);
+		System.out.println();
+		
+		sc.close();
+		
+	}
+
+}
